2a97b9029b43de62399a7a798274648194531fe7
Rustam Botirov <rustambotirov88@gmail.com>
2019-11-27 23:08:21 +0200

    use redux

--- src/index.js
+++ src/index.js
@@ -4,19 +4,32 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return state;
     }
 };
 
 const store = createStore(reducer);
-store.subscribe(()=>{
-    console.log(store.getState()); 
-});
+const counter = document.querySelector('#counter');
 
-store.dispatch({type:'INC'});
-store.dispatch({type:'INC'});
+const inc = () => {
+    store.dispatch({ type: 'INC' });
+};
+const dec = () => {
+    store.dispatch({ type: 'DEC' })
+};
 
+const update = () => {
+    counter.textContent = store.getState();
+};
 
-
+store.subscribe(update)
+document
+    .querySelector('.inc')
+    .addEventListener('click', inc);
+document
+    .querySelector('.dec')
+    .addEventListener('click', dec);
 
