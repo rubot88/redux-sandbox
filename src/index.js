b37743c31636d1aa00682e990781ae7050403dc1
Rustam Botirov <rustambotirov88@gmail.com>
2019-11-28 22:23:25 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ const { dispatch } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-const counter = document.querySelector('#counter');
-const update = () => {
-    counter.textContent = store.getState();
-};
-
-store.subscribe(update)
 document
     .querySelector('.inc')
     .addEventListener('click', inc);
@@ -26,4 +20,8 @@ document
         const randomValue = Math.floor(Math.random() * 10);
         rnd(randomValue);
     });
-
+const update = () => {
+    document.querySelector('#counter')
+        .textContent = store.getState();
+};
+store.subscribe(update);
